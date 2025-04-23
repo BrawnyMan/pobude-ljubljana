63290eb9e8352b7857b1f6a07e85e946df6ca666
BrawnyMan <matic.podlesek@gmail.com>
2025-04-23 23:44:00 +0200

    React app to Vite app (also React)

--- frontend/src/components/MapPlaceholder.jsx
+++
@@ -1,7 +0,0 @@
-export default function MapPlaceholder() {
-    return (
-        <div style={{ height: "400px", backgroundColor: "#ddd", textAlign: "center", paddingTop: "180px" }}>
-            <strong>Zemljevid (bo implementiran)</strong>
-        </div>
-    );
-}
--- frontend/src/components/PobudaCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function PobudaCard({ pobuda }) {
-    return (
-        <div style={{ border: "1px solid #ccc", padding: "1em", marginBottom: "1em" }}>
-            <h3>{pobuda.naslov}</h3>
-            <p>{pobuda.vsebina}</p>
-            <small>Status: {pobuda.status}</small>
-        </div>
-    );
-}
