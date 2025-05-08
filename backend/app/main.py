73827199a8e6970dd6714ffca650fa8c884f5fb4
BrawnyMan <matic.podlesek@gmail.com>
2025-05-08 23:51:04 +0200

    Updated BE for creating custom "pobude"

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI
 from sqlmodel import Field, SQLModel, create_engine, Session, select
-from typing import Optional, List
-import random
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException
+from typing import Optional, List
 
 app = FastAPI()
 
@@ -16,40 +15,31 @@ app.add_middleware(
 
 class Pobuda(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    naslov: str
-    vsebina: str
+    title: str
+    description: str
+    location: str
+    email: str
+    captcha: Optional[str] = None
     status: Optional[str] = "v obravnavi"
 
 DATABASE_URL = "sqlite:///pobude.db"
 engine = create_engine(DATABASE_URL, echo=True)
 
+# --- Remove database and create new one ---
+# SQLModel.metadata.drop_all(engine)
+
 SQLModel.metadata.create_all(engine)
 
-@app.post("/ustvari-nakljucno", response_model=Pobuda)
-def ustvari_nakljucno_pobudo():
-    naslovi = ["Nova kolesarska pot", "Preveč parkiranih vozil", "Slaba osvetlitev", "Zelena pobuda"]
-    vsebine = [
-        "Predlagam kolesarsko pot ob Ljubljanici.",
-        "Premalo nadzora parkiranja v centru.",
-        "Ulica ponoči ni dovolj osvetljena.",
-        "Več dreves ob mestnih vpadnicah."
-    ]
-
-    nova_pobuda = Pobuda(
-        naslov=random.choice(naslovi),
-        vsebina=random.choice(vsebine),
-        status="v obravnavi"
-    )
-
+@app.post("/pobude", response_model=Pobuda)
+def dodaj_pobudo(pobuda: Pobuda):
     with Session(engine) as session:
-        session.add(nova_pobuda)
+        session.add(pobuda)
         session.commit()
-        session.refresh(nova_pobuda)
-        return nova_pobuda
+        session.refresh(pobuda)
+        return pobuda
 
 @app.get("/pobude", response_model=List[Pobuda])
-def preberi_vse_pobude():
+def preberi_pobude():
     with Session(engine) as session:
         statement = select(Pobuda)
-        rezultati = session.exec(statement).all()
-        return rezultati
+        return session.exec(statement).all()
