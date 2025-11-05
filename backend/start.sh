780838c6d5c05693136f2f371e6fe4072b343893
BrawnyMan <matic.podlesek@gmail.com>
2025-11-05 22:33:11 +0100

    Chaged start.sh + .gitignore

--- backend/start.sh
+++ backend/start.sh
@@ -3,10 +3,9 @@
 if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
     echo "🔧 Linux/macOS detected – activating venv/bin/activate"
     source ./venv/bin/activate
+    ./venv/bin/python -m uvicorn app.main:app --reload
 else
     echo "🪟 Windows detected – activating venv/Scripts/activate"
     source ./venv/Scripts/activate
+    ./venv/Scripts/python.exe -m uvicorn app.main:app --reload
 fi
-
-uvicorn app.main:app --reload
-# uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
